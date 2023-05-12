6903358ad28794fa806de311e6298f66d821028d
Gusb <mihivanmih@gmail.com>
2023-05-12 22:54:06 +0300

    bug react

--- pages/fairy/index.tsx
+++ pages/fairy/index.tsx
@@ -1,8 +1,21 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import MainLayout from '@/layots/MainLayout';
 import styles from '@/styles/fairy/fairy.module.scss'
 
+
+
+
 const Witcher: FC = () => {
+
+    // @ts-ignore
+    useEffect(() => {
+        window.addEventListener('scroll', e => {
+            document.body.style.setProperty(
+                "--scrollTopt",
+                `${windowSize.width - 20}px`
+            )
+        })
+    }, [windowSize])
 
     return (
         <>
@@ -18,13 +31,13 @@ const Witcher: FC = () => {
                                     Fairy Forest
                                 </div>
                             </div>
-                            <div className={styles.layer} style={{backgroundImage: `url('/img/fairy/layer-base.png')`}}> </div>
+                            <div className={`${styles.layer} layer_base`} style={{backgroundImage: `url('/img/fairy/layer-base.png')`}}> </div>
                             <div className={styles.layer} style={{backgroundImage: `url('/img/fairy/layer-middle.png')`}}> </div>
                             <div className={styles.layer} style={{backgroundImage: `url('/img/fairy/layer-front.png')`}}> </div>
                         </div>
                     </header>
 
-                    <article className={"main"}>
+                    <article className={styles.main} style={{backgroundImage: `url('/img/fairy/dungeon.jpg')`}}>
                         <div className={"content"}>
                             <h2 className={"header"}>To be continued</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad autem beatae culpa dicta dignissimos dolorum ipsa iste laboriosam, magnam, molestiae non numquam optio placeat praesentium soluta. A dolore eaque minima molestiae quam!</p>
