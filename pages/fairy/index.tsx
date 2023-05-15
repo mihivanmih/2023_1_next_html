b208d13a05a625b1924eb2477d0f242695ab6ea8
Gusb <mihivanmih@gmail.com>
2023-05-15 22:27:07 +0300

    finish

--- pages/fairy/index.tsx
+++ pages/fairy/index.tsx
@@ -2,9 +2,6 @@ import React, {FC, useEffect, useState} from 'react';
 import MainLayout from '@/layots/MainLayout';
 import styles from '@/styles/fairy/fairy.module.scss'
 
-
-
-
 const Witcher: FC = () => {
     const [offset, setOffset] = useState(0);
 
@@ -28,37 +25,40 @@ const Witcher: FC = () => {
         )
     }
 
-
-
     return (
         <>
             <MainLayout title={'Fairy forest'}>
-                <div className={styles.fairy}>
-                    <header className={styles.mainHeader}>
-                        <div className={styles.layers}>
-                            <div className={styles.header}>
-                                <div className={styles.caption}>
-                                    Welcome to Parallax
+                <div className="wrapper">
+                    <div className="content">
+                        <div className={styles.fairy}>
+                            <header className={styles.mainHeader}>
+                                <div className={styles.layers}>
+                                    <div className={styles.header}>
+                                        <div className={styles.caption}>
+                                            Welcome to Parallax
+                                        </div>
+                                        <div className={styles.title}>
+                                            Fairy Forest
+                                        </div>
+                                    </div>
+                                    <div className={`${styles.layer} ${styles.layer_base}`} style={{backgroundImage: `url('/img/fairy/layer-base.png')`}}> </div>
+                                    <div className={`${styles.layer}  ${styles.layer_middle}`} style={{backgroundImage: `url('/img/fairy/layer-middle.png')`}}> </div>
+                                    <div className={`${styles.layer}  ${styles.layer_front}`} style={{backgroundImage: `url('/img/fairy/layer-front.png')`}}> </div>
                                 </div>
-                                <div className={styles.title}>
-                                    Fairy Forest
+                            </header>
+
+                            <article className={styles.main} style={{backgroundImage: `url('/img/fairy/dungeon.jpg')`}}>
+                                <div className={"content"}>
+                                    <h2 className={styles.header}>To be continued</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad autem beatae culpa dicta dignissimos dolorum ipsa iste laboriosam, magnam, molestiae non numquam optio placeat praesentium soluta. A dolore eaque minima molestiae quam!</p>
                                 </div>
-                            </div>
-                            <div className={`${styles.layer} ${styles.layer_base}`} style={{backgroundImage: `url('/img/fairy/layer-base.png')`}}> </div>
-                            <div className={`${styles.layer}  ${styles.layer_middle}`} style={{backgroundImage: `url('/img/fairy/layer-middle.png')`}}> </div>
-                            <div className={`${styles.layer}  ${styles.layer_front}`} style={{backgroundImage: `url('/img/fairy/layer-front.png')`}}> </div>
-                        </div>
-                    </header>
 
-                    <article className={styles.main} style={{backgroundImage: `url('/img/fairy/dungeon.jpg')`}}>
-                        <div className={"content"}>
-                            <h2 className={"header"}>To be continued</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad autem beatae culpa dicta dignissimos dolorum ipsa iste laboriosam, magnam, molestiae non numquam optio placeat praesentium soluta. A dolore eaque minima molestiae quam!</p>
+                                <div className={styles.copy}>© WebDesign Master</div>
+                            </article>
                         </div>
-
-                        <div className={"copy"}>© WebDesign Master</div>
-                    </article>
+                    </div>
                 </div>
+
 
             </MainLayout>
         </>
