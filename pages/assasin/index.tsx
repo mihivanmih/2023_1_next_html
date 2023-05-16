b02630d4b9c20c05511664b9183c51a2afda0b6a
Gusb <mihivanmih@gmail.com>
2023-05-16 23:02:47 +0300

    start assasin

---
+++ pages/assasin/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainLayout from "@/layots/MainLayout";
+import styles from '@/styles/assasin/assasin.module.scss'
+
+
+const Assasin = () => {
+     return (
+        <MainLayout title={"Assasin's Creed"} className={`${styles.auth}`}>
+           d
+        </MainLayout>
+    );
+};
+
+export default Assasin;
